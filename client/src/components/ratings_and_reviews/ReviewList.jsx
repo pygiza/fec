0abb537bef30e693ae3e0c9b7e41612e39a99bb4
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-05-19 19:25:59 -0400

    make ratings and reviews component folder

---
+++ client/src/components/ratings_and_reviews/ReviewList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReviewEntry from './ReviewEntry.jsx';
+
+function ReviewList() {
+  return (
+    <div>
+      <ReviewEntry />
+      <ReviewEntry />
+    </div>
+  );
+}
+
+export default ReviewList;
