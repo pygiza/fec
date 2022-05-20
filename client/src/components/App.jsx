3c54e277baddb67b921d3815ef94fb56ee9886e2
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-05-20 10:32:07 -0400

    Add container components to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import Overview from './overview/Overview.jsx';
+import RelatedProductsContainer from './relatedProducts/RelatedProductsContainer.jsx';
+import Question from './qComponents/questions.jsx';
+import RatingsReviews from './ratings_and_reviews/RatingsReviews.jsx';
+
 
 function App(props) {
   return (
     <div>
-      <h1>Hello World, we're coming for you!</h1>
+      <h1>PyGiza</h1>
       <Overview />
+      <RelatedProductsContainer />
+      <Question />
+      <RatingsReviews />
     </div>
   );
 }
