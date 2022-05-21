21c61a5b791861279ee309b3c6b2e5b8ff7a8530
LWcaveman <levi_82@hotmail.com>
2022-05-21 15:26:50 -0500

    pre style-loader

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,10 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  color: white;
+`;
 
 function Overview() {
-  const [images, setImages] = useState([]);
+  const [styles, setStyles] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const fetchStyles = (product) => {
+    console.log('BIG P ', products.id);
+    axios.get(`http://localhost:3000/products/${product.id}/styles`)
+      .then((data) => {
+        console.log(data.data.results);
+        setStyles((prevState) => [...prevState, data.data.results]);
+      });
+  };
+
+  const fetchData = () => {
+    axios.get('http://localhost:3000/products')
+      .then((data) => {
+        console.log('DATA DATA => ', data.data);
+        setProducts(data.data);
+        data.data.forEach((product) => {
+          fetchStyles(product);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    //fetchData();
+  }, []);
 
   return (
-    <dvi className="overview"> YEAH I'm Overview Container </dvi>
+    <Container>
+      {/*       Soon there will be images!
+      {products.map((product) => (
+        <li>
+        {product.name}
+        {/* {styles.map((style) => {
+        })} }
+        </li>
+      ))} */}
+    </Container>
   );
 }
 export default Overview;
--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "client/src", "index.html"),
     }),
+    ['styled-components', { 'ssr': true }],
   ],
 }
