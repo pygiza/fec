65e0d58a62751034860503d38116162eab559a96
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-05-25 00:26:15 +0900

    Initialized CarouselButton component

--- client/src/components/relatedProducts/CarouselCard/CarouselTop/CarouselTop.jsx
+++ client/src/components/relatedProducts/CarouselCard/CarouselTop/CarouselTop.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import CarouselButton from './CarouselButton.jsx';
 
 const Top = styled.div`
   display: flex;
   justify-content: center;
   height: 60%;
+  position: relative;
   -webkit-mask-image: -webkit-gradient(linear, left 20%, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
   mask-image: -webkit-gradient(linear, left 20%, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
 `
@@ -18,6 +20,7 @@ width: 100%;
 const CarouselTop = function({ image, characteristics }) {
   return (
     <Top>
+      <CarouselButton />
       <Thumbnail src={image}></Thumbnail>
     </Top>
   )
