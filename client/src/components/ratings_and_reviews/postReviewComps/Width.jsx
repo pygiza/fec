1285d34d8ce883ed2ebe31a145dbd467d8dc673a
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-05-26 16:34:32 -0400

    Create file structure for radio button arrays

---
+++ client/src/components/ratings_and_reviews/postReviewComps/Width.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function Width () {
+  return (
+    <div></div>
+  );
+}
+
+export default Width;
--- client/src/components/ratings_and_reviews/reviewListComps/WriteReviewForm.jsx
+++ client/src/components/ratings_and_reviews/reviewListComps/WriteReviewForm.jsx
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 function WriteReviewForm({ metaData }) {
+  var charForms = [];
+  for (let key in metaData.characteristics) {
+
+  }
   return (
     <div>
       <h3>Write Your Review</h3>
       <h4>About the PRODUCT NAME</h4>
       {/* Overall Rating */}
       <p>Do you recommend this product?</p>
-      <input type="radio" name="rec" value="true" id="yesRec" />
+      <input type="radio" name="recommend" value="true" id="yesRec" />
       <label htmlFor="yesRec">Yes</label>
-      <input type="radio" name="rec" value="false" id="noRec" />
+      <input type="radio" name="recommend" value="false" id="noRec" />
       <label htmlFor="noRec">No</label>
 
       <h4>Characteristics</h4>
