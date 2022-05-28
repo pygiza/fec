a39030197a61760709d1fada81d19adfb7eb071d
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-05-28 10:19:30 -0400

    Set up breakdown state object

--- client/src/components/ratings_and_reviews/ReviewBreakdown.jsx
+++ client/src/components/ratings_and_reviews/ReviewBreakdown.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
-const ReviewBreakdownContainer = styled.div`
-  width: 34%;
-  margin: 2%;
-  border: solid black 1px;
-`;
+import { getMetaData } from './serverFuncs.js';
 
 function ReviewBreakdown({productId}) {
+  const [ratings, setRatings] = useState({});
+
+  function getRatings() {
+    getMetaData(productId)
+      .then((data) => {
+        setRatings(data);
+      });
+  }
+
+  useEffect(() => {
+    setRatings({});
+    getRatings();
+  }, [productId]);
+
   return (
     <ReviewBreakdownContainer>
       This is the ReviewBreakdown
@@ -19,5 +28,11 @@ function ReviewBreakdown({productId}) {
 ReviewBreakdown.propTypes = {
   productId: PropTypes.number.isRequired,
 };
+
+const ReviewBreakdownContainer = styled.div`
+  width: 34%;
+  margin: 2%;
+  border: solid black 1px;
+`;
 
 export default ReviewBreakdown;
