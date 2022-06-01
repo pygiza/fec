6a5649a472e8bab23189039d9ed9e4f08e747e3a
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-02 08:41:03 +0900

    Include examples for button clicks and asnyc function

--- ExampleComponent.jsx
+++ ExampleComponent.jsx
@@ -4,9 +4,21 @@ const ExampleComponent = function(props) {
 
   let [text, setText] = useState('some text');
 
+  let updateText = function() {
+    setText('some more text');
+  }
+
+  let asyncUpdateText = function() {
+    setTimeout(() => {
+      setText('some even more text');
+    }, 1000)
+  }
+
   return(
     <>
       <p data-testid='text'>{text}</p>
+      <button data-testid='button' onClick={updateText} />
+      <button data-testid='async-button' onClick={asyncUpdateText} />
     </>
   )
 }
--- example.test.js
+++ example.test.js
@@ -7,7 +7,26 @@ import { render, fireEvent } from '@testing-library/react';
 import 'regenerator-runtime';
 import ExampleComponent from './ExampleComponent.jsx';
 
-it('should render component', () => {
-  let { getByTestId } = render(<ExampleComponent />);
-  expect(getByTestId('text')).toHaveTextContent('some text');
-})
+describe('example component', () => {
+
+  it('should have text', () => {
+    let { getByTestId } = render(<ExampleComponent />);
+    expect(getByTestId('text')).toHaveTextContent('some text');
+  });
+
+  it('should change text when button clicked', () => {
+    let { getByTestId } = render(<ExampleComponent />);
+
+    fireEvent.click(getByTestId('button'));
+
+    expect(getByTestId('text')).toHaveTextContent('some more text');
+  });
+
+  if('should change text when async button clicked', async () => {
+    let {getByTestId } = render(<ExampleComponent />);
+
+    await fireEvent.click(getByTestId('async-button'));
+
+    expect(getByTestId('text')).toHaveTextContent('some even more text');
+  });
+});
