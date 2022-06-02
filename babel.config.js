01b3195d2f0c3b737faf25f535c0630453734679
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-03 00:03:53 +0900

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   presets: ['@babel/preset-env', '@babel/preset-react'],
-=======
-  presets: [
-    '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
->>>>>>> master
 };
